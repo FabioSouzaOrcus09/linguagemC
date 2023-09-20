ef41a9c58c0a33894533b9f18aef2c2ed14bd19c
Fabio de souza andrade <fabio.andrade46@etec.sp.gov.br>
2023-09-20 17:41:09 -0300

    arquivo da aula do dia 20/09/2023 sobre linguagemC

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("jefferson é o melhor professor de todos \n");
+	printf("ele é gentil\n");
+	printf("ele explica muito bem\n");
+		printf("ele é meu hero\n");
+	return 0;	
+}
+
